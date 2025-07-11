5361ad0ed8ae6329d5f81b70ee1952605fe7d75e
stinger13 <93597591+stinger13@users.noreply.github.com>
2025-07-12 01:10:54 +0530

    responsive sidebar

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,7 @@
 import CreateTask from './ui/front-page/create-task/CreateTask'
+import SideBar from './ui/front-page/create-task/SideBar'
 
 export {
-    CreateTask
+    CreateTask,
+    SideBar
 }
