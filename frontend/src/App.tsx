f99fcf2099bda615c395b29bfbde3f89097913d1
Sahil Raturi <sahilraturi687@gmail.com>
2025-07-12 14:49:34 +0530

    MainLayout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,9 @@
-import { CreateTask, SideBar } from "./components"
+import { MainLayout } from "./components"
 
 function App() {
   return (
-    // gradient just for testing
-    <div className="flex min-h-svh from-blue-900 to-blue-950 bg-gradient-to-l p-4"> 
-      <SideBar/>
-      <div className="flex w-full h-full justify-center items-center">
-        <CreateTask />
-      </div>
+    <div>
+      <MainLayout />
     </div>
   )
 }
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,7 +1,9 @@
 import CreateTask from './ui/front-page/create-task/CreateTask'
 import SideBar from './ui/front-page/create-task/SideBar'
+import MainLayout from './ui/front-page/create-task/MainLayout'
 
 export {
     CreateTask,
-    SideBar
+    SideBar,
+    MainLayout
 }
