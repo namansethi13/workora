a0c62304208d6a615f6a02fdbe1ba00fbd95676a
Ananya Aggarwal <aggarwalananya03@gmail.com>
2025-07-16 18:52:41 +0530

    Added Admin page sidebar Functionality

---
+++ frontend/src/components/ui/admin-page/SidebarToggler.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+
+type Props = {
+  isOpen: boolean;
+  setIsOpen: (isOpen : boolean) => void;
+}
+
+function SidebarToggler({isOpen, setIsOpen}: Props) {
+  return (
+    <div>
+      <button onClick={() => setIsOpen(!isOpen)} className='bg-custom-gray rounded-r-xl h-20 flex items-center justify-center hover:bg-custom-gray-400 dark:bg-white/10 m-0'>
+        {isOpen ? <ChevronLeft /> : <ChevronRight />}
+      </button>
+    </div>
+  )
+}
+
+export default SidebarToggler
+
