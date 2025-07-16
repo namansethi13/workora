88de46633ece9969c87cb6e968d29ce23090c26d
Ananya Aggarwal <aggarwalananya03@gmail.com>
2025-07-16 18:52:41 +0530

    Added Admin page sidebar Functionality

--- frontend/src/components/ui/dashboard/Dashboard.tsx
+++ frontend/src/components/ui/dashboard/Dashboard.tsx
@@ -7,13 +7,12 @@ export default function Dashboard(){
 
     return (
         <div className="w-[100svw] flex min-h-svh bg-custom-white dark:bg-custom-blue">
-        
-                    <aside className="h-svh w-fit p-4">
-                        <Sidebar />
-                    </aside>
-                    <div className="absolute top-10 right-15 z-50">
-                        <Toggler />
-                    </div>
+            <aside className="h-svh w-fit p-4">
+                <Sidebar />
+            </aside>
+            <div className="absolute top-10 right-15 z-50">
+                <Toggler />
+            </div>
         </div>
     )
 
---
+++ frontend/src/pages/AdminPage.tsx
@@ -0,0 +1,28 @@
+import { Sidebar, SidebarToggler, Toggler } from '../components'
+import { useState } from 'react'
+
+function AdminPage() {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+
+  return (
+    <div className='flex min-h-screen bg-white dark:bg-custom-blue relative overflow-hidden'>
+        <aside className={`relative flex flex-row transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'}`} style={{ width: '100px' }}>
+                <Sidebar/>
+                <div className='flex items-center'>
+                    <SidebarToggler isOpen={isOpen} setIsOpen={setIsOpen}/>
+                </div>
+        </aside>
+
+        <main className={`flex-1 transtion-all duration-300 ease-in-out ${isOpen ? 'ml-[90px]' : 'ml-0'}`}>
+            <div className="absolute top-10 right-15 z-50">
+                <Toggler />
+            </div>
+            <h1>Admin page</h1>
+        </main>
+    </div>
+  )
+}
+
+export default AdminPage
